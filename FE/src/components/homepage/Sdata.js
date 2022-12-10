d2d3c6c5cea39ff837d811821a9a7e1e1138f521
TangTriDuc <19521382@gm.uit.edu.vn>
2022-12-10 17:48:39 +0700

    login and register and all finish 30%

---
+++ FE/src/components/homepage/Sdata.js
@@ -0,0 +1,28 @@
+const Sdata = [
+        {
+            id: 1,
+            title: "Meticulous in every detail",
+            desc: "With leading technology & strict supervision process, Japanese figures have the soul of the original drawings, vivid aura, sharp details from shape to color.",
+            cover: 'https://animestore.vn/wp-content/uploads/2021/09/mo-hinh-goku.jpg'
+        },
+        {
+            id: 2,
+            title: "For beginners",
+            desc: "How to distinguish real from fake, How to track the release schedule, How to clean the Figure, How to paint",
+            cover: 'https://animestore.vn/wp-content/uploads/2021/09/1.png'
+        },
+        {
+            id: 3,
+            title: "When will a Figure be available in Japan and back to Vietnam?",
+            desc: "Waiting to have and be updated information on fanpage and allow booking date",
+            cover: 'https://animestore.vn/wp-content/uploads/2021/09/2-1.png'
+        },
+        {
+            id: 4,
+            title: "Place of exchange and warranty Figure",
+            desc: "Fig you need someone else to have, Fig you have someone else need",
+            cover: 'https://animestore.vn/wp-content/uploads/2021/09/4-1.png'
+        }
+    
+    ]
+export default Sdata;
